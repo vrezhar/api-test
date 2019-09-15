f9d0cb4a2bf6da3a23ba8f7a4a92b95b55bc249a
vrezhar <vrarakel01@gmail.com>
2019-09-16 03:01:43 +0400

    working pseudo-api

---
+++ src/test/groovy/restapi/ControlledDataSpec.groovy
@@ -0,0 +1,18 @@
+package restapi
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class ControlledDataSpec extends Specification implements DomainUnitTest<ControlledData> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ src/test/groovy/secured/DictionaryEntrySpec.groovy
@@ -0,0 +1,19 @@
+package secured
+
+import grails.testing.gorm.DomainUnitTest
+import restapi.DictionaryEntry
+import spock.lang.Specification
+
+class DictionaryEntrySpec extends Specification implements DomainUnitTest<DictionaryEntry> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
